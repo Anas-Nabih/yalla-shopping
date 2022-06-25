2e7457c99d47e4a55b7e33bfa4a816351843ab0a
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-06-25 17:49:46 +0200

    first commit

---
+++ lib/app/my_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatefulWidget {
+
+  MyApp._internal();
+
+  int appState = 0;
+  //singleton or single instance
+  static final MyApp _instance = MyApp._internal();
+
+  factory MyApp()=>_instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:yalla_shopping/app/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
