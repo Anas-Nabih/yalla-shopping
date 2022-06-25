283ec85bb7241dd69e1ccf4202396a5c58a6b229
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-06-25 20:07:04 +0200

    add route manager

---
+++ lib/presentation/res/routes.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yalla_shopping/presentation/main/main_view.dart';
+import 'package:yalla_shopping/presentation/splash/splash_view.dart';
+import 'package:yalla_shopping/presentation/store_details/store_details_view.dart';
+import 'package:yalla_shopping/presentation/user/forget_password/forget_password_view.dart';
+import 'package:yalla_shopping/presentation/user/login/login_view.dart';
+import 'package:yalla_shopping/presentation/user/register/register_view.dart';
+
+class Routes{
+  static const String splashRoute = "/";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgetPasswordRoute = "/forgetPassword";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator{
+  static Route<dynamic> getRoute(RouteSettings settings){
+    switch(settings.name){
+      case Routes.splashRoute:
+        return CupertinoPageRoute(builder: (context) => const SplashView(),);
+      case Routes.loginRoute:
+        return CupertinoPageRoute(builder: (context) => const LoginView(),);
+      case Routes.registerRoute:
+        return CupertinoPageRoute(builder: (context) => const RegisterView(),);
+      case Routes.forgetPasswordRoute:
+        return CupertinoPageRoute(builder: (context) =>const  ForgetPasswordView(),);
+      case Routes.mainRoute:
+        return CupertinoPageRoute(builder: (context) => const MainView(),);
+      case Routes.storeDetailsRoute:
+        return CupertinoPageRoute(builder: (context) => const StoreDDetailsView(),);
+     default:
+    return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute ()=>CupertinoPageRoute(builder: (context) => Scaffold(
+    appBar: AppBar(title: const Text("No Route Find")),
+    body:const Center(child:Text("PLease check your navigation route")),
+  ),);
+}
---
+++ lib/presentation/store_details/store_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class StoreDDetailsView extends StatelessWidget {
+  const StoreDDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
