8257e8b336c69006611eba92d05628ff13225a84
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-06-29 23:43:12 +0200

    handle onboarding view model inputs and outputs

--- lib/presentation/onboarding/view_model/onboarding_view_model.dart
+++ lib/presentation/onboarding/view_model/onboarding_view_model.dart
@@ -1,6 +1,10 @@
 import 'package:yalla_shopping/presentation/base/base_view_model.dart';
 
-class OnBoardingViewModel extends BaseViewModel{
+class OnBoardingViewModel extends BaseViewModel with OnBoardingViewModelInputs, OnBoardingViewModelOutputs{
+
+  /// stream controller outputs
+
+ /// Onboarding view model inputs
   @override
   void dispose() {
     // TODO: implement dispose
@@ -10,7 +14,27 @@ class OnBoardingViewModel extends BaseViewModel{
   void start() {
     // TODO: implement start
   }
+
+  @override
+  void goNext() {
+    // TODO: implement goNext
+  }
+
+  @override
+  void goPrevious() {
+    // TODO: implement goPrevious
+  }
+
+  @override
+  void onPageChange(int index) {
+    // TODO: implement onPageChange
+  }
 }
 
-abstract class OnBoardingViewModelInputs{}
+
+ abstract class OnBoardingViewModelInputs{
+  void goNext();
+  void goPrevious();
+  void onPageChange(int index);
+ }
 abstract class OnBoardingViewModelOutputs{}
