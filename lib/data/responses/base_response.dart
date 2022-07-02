a6473fe21618088b4c4bd3c65bda2782a16792c9
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-07-02 15:36:14 +0200

    Adding APIs Responses

---
+++ lib/data/responses/base_response.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'base_response.g.dart';
+
+
+@JsonSerializable()
+class BaseResponse{
+  @JsonKey(name:"status")
+  int? status;
+  @JsonKey(name:"message")
+  String? message;
+  
+ }
+
+
+@JsonSerializable()
+class CustomerResponse{
+  @JsonKey(name:"id")
+  String? id;
+  @JsonKey(name:"name")
+  String? name;
+  @JsonKey(name:"email")
+  String? email;
+  @JsonKey(name:"phone")
+  String? phone;
+  @JsonKey(name:"role")
+  String? role;
+  @JsonKey(name:"numOfNotifications")
+  int? numOfNotifications;
+  
+  CustomerResponse(this.id,this.name,this.email,this.phone,this.role,this.numOfNotifications);
+
+  /// from json
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) => _$CustomerResponseFromJson(json);
+
+  /// to json
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
+}
+
+
+@JsonSerializable()
+class LoginResponse extends BaseResponse{
+  @JsonKey(name:"customer")
+  CustomerResponse? customer;
+  
+  LoginResponse(this.customer);
+
+  /// from json
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => _$LoginResponseFromJson(json);
+
+  /// to json
+  Map<String, dynamic> toJson() => _$LoginResponseToJson(this);
+}
---
+++ lib/data/responses/base_response.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'base_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BaseResponse _$BaseResponseFromJson(Map<String, dynamic> json) => BaseResponse()
+  ..status = json['status'] as int?
+  ..message = json['message'] as String?;
+
+Map<String, dynamic> _$BaseResponseToJson(BaseResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+    };
+
+CustomerResponse _$CustomerResponseFromJson(Map<String, dynamic> json) =>
+    CustomerResponse(
+      json['id'] as String?,
+      json['name'] as String?,
+      json['email'] as String?,
+      json['phone'] as String?,
+      json['role'] as String?,
+      json['numOfNotifications'] as int?,
+    );
+
+Map<String, dynamic> _$CustomerResponseToJson(CustomerResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'phone': instance.phone,
+      'role': instance.role,
+      'numOfNotifications': instance.numOfNotifications,
+    };
+
+LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
+    LoginResponse(
+      json['customer'] == null
+          ? null
+          : CustomerResponse.fromJson(json['customer'] as Map<String, dynamic>),
+    )
+      ..status = json['status'] as int?
+      ..message = json['message'] as String?;
+
+Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'customer': instance.customer,
+    };
