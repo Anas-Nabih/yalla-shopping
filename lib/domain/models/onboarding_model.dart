3aaa79d201a22edebe612ef9b258f489836836ee
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-07-03 23:39:26 +0200

    create nullable extensions

--- lib/domain/models/onboarding_model.dart
+++ lib/domain/models/onboarding_model.dart
@@ -14,3 +14,17 @@ class SliderViewObject{
 
   const SliderViewObject({required this.sliderObj,required this.currentIndex,required this.numOfSlides});
 }
+
+
+/// login model
+
+class Authentication {
+   String  id;
+   String  name;
+   String  email;
+   String  phone;
+   String  role;
+   int  numOfNotifications;
+
+   Authentication({required this.email,required this.name,required this.phone,required this.id,required this.role,required this.numOfNotifications});
+}
